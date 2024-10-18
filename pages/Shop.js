9d6810d75f719e36ab026519462d22066d944ed8
deltadash2 <deltadash2@outlook.com>
2024-10-18 14:57:25 -0400

    upload new product

--- pages/Shop.js
+++ pages/Shop.js
@@ -18,7 +18,7 @@ import axios from "axios";
 import { useEffect } from "react";
 import Product from "@/MyComponents/Product";
 
-const PUBLISHED_PRODUCTS_CNT = 30;
+const PUBLISHED_PRODUCTS_CNT = 31;
 
 const Shop = ({ headerFooter, products }) => {
   const publishedProducts = products.slice(
